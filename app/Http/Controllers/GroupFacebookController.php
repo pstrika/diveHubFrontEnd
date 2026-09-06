1b5e4355bc532f6e301591be46980b76443dbb89
Pablo Strika <pstrika@gmail.com>
2026-09-06 02:09:11 -0400

    9.7.2 09/06/26 - fix: Facebook connect missing pages_read_engagement scope
    pages_manage_posts has a hard dependency on pages_read_engagement -
    Meta rejects the whole OAuth request if it's requested without it.
    Added it to the scope list alongside pages_show_list/pages_manage_posts.

--- app/Http/Controllers/GroupFacebookController.php
+++ app/Http/Controllers/GroupFacebookController.php
@@ -28,7 +28,7 @@ class GroupFacebookController extends Controller
         session(['fb_connect_group_id' => $group->id]);
 
         return Socialite::driver('facebook')
-            ->setScopes(['pages_show_list', 'pages_manage_posts'])
+            ->setScopes(['pages_show_list', 'pages_manage_posts', 'pages_read_engagement'])
             ->redirect();
     }
 
